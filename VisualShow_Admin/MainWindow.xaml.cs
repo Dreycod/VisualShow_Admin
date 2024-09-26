2b54340cd9e76b92a8f7e8adb19e127a33ef6dad
Drey <84332780+Dreycod@users.noreply.github.com>
2024-09-26 15:06:16 +0200

    UI

--- VisualShow_Admin/MainWindow.xaml.cs
+++ VisualShow_Admin/MainWindow.xaml.cs
@@ -14,11 +14,46 @@ namespace VisualShow_Admin
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void NavigationViewItem_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
