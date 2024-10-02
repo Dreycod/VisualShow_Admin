d7c545bc09eb83513a828cc53b08a0d341fec2a1
Akiel <122918848+Akieldamas@users.noreply.github.com>
2024-10-02 17:23:06 +0200

    ftp tests

---
+++ VisualShow_Admin/Controller/FTPServer_Manager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualShow_Admin.Controller
+{
+    public class FTPServer_Manager
+    {
+        public FTPServer_Manager()
+        {
+        
+        }
+
+        public void UploadToFTPServer(string filename)
+        {
+            FtpWebRequest request =
+            (FtpWebRequest)WebRequest.Create("ftp://ftp-borne-arcade.alwaysdata.net/Images/KM103/"+filename);
+            request.Credentials = new NetworkCredential("borne-arcade", "borne-testing");
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+
+            using(Stream fileStream = File.OpenRead(filename))
+            using (Stream ftpStream = request.GetRequestStream())
+            {
+                fileStream.CopyTo(ftpStream);
+            }
+        }
+
+
+
+    }
+}
--- VisualShow_Admin/View/Page_Media.xaml.cs
+++ VisualShow_Admin/View/Page_Media.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VisualShow_Admin.Controller;
 
 namespace VisualShow_Admin.View
 {
@@ -20,19 +21,41 @@ namespace VisualShow_Admin.View
     /// </summary>
     public partial class Page_Media : Page
     {
+        FTPServer_Manager ftpServer_Manager;
+        string filename;
         public Page_Media()
         {
             InitializeComponent();
+            ftpServer_Manager = new FTPServer_Manager();
+
         }
 
         private void UploadFile_Click(object sender, RoutedEventArgs e)
         {
+            // communicate with FTP server using settings from the settings view page
+            if (filename != null)
+            {
+                ftpServer_Manager.UploadToFTPServer(filename);
+            }
+            else
+            {
+                MessageBox.Show("Please select a file to upload");
+            }
+
+
 
         }
 
         private void BrowseFile_Click(object sender, RoutedEventArgs e)
         {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".jpg, .png";
+            dlg.Filter = "Image Files (*.jpg, *.png)|*.jpg;*.png";
+            Nullable<bool> result = dlg.ShowDialog();
 
+            filename = dlg.FileName;
+            FilePathTextBox.Text = filename;
+            MessageBox.Show(filename);
         }
     }
 }
