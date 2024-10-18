1d5ddf21ae7ebd39b252a4696135f336097fd45f
matheoalexis long <matheoalexis.long@saintmichelannecy.fr>
2024-10-18 16:50:26 +0200

    oui

--- VisualShow_Admin/View/Page_News.xaml.cs
+++ VisualShow_Admin/View/Page_News.xaml.cs
@@ -33,14 +33,108 @@ namespace VisualShow_Admin.View
 
         public void BroadcastMessage_Click(object sender, RoutedEventArgs e)
         {
-            int num = MessagesListBox.Items.Count;
-            num += 1;
-            string cheminTopic = "KM103/emergency";
-            string message = MessageInput.Text;
-            prout.PublishTopicMessage(cheminTopic, message);
-            ListBoxItem machin = new ListBoxItem();
-            machin.Content = "Message " + num + ": " + message;
-            MessagesListBox.Items.Add(machin);
+            try
+            {
+                if (ScreenComboBox.SelectedItem != null)
+                {
+                    int num = MessagesListBox.Items.Count;
+                    num += 1;
+                    string cheminTopic = ScreenComboBox.SelectedItem.ToString() + "/emergency";
+                    string message = MessageInput.Text;
+                    prout.PublishTopicMessage(cheminTopic, message);
+                    ListBoxItem machin = new ListBoxItem();
+                    machin.Content = "Message " + num + ": " + message;
+                    MessagesListBox.Items.Add(machin);
+                }
+                else
+                {
+                    MessageBox.Show("Please select a screen to sent the message to", "Error, no screen selected", MessageBoxButton.OK);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An unexpected error occurred", "Error", MessageBoxButton.OK);
+            }
+        }
+
+        private async void ScreenComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedScreen = ScreenComboBox.SelectedItem.ToString();
+        }
+
+        //publication préfaite d'un message d'urgence (incendie) sur le topic "emergency" du screen selectionné
+        private void FireAlert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (ScreenComboBox.SelectedItem != null)
+                {
+                    string cheminTopic = ScreenComboBox.SelectedItem.ToString() + "/emergency";
+                    string message = "FireAlert";
+                    prout.PublishTopicMessage(cheminTopic, message);
+                    ListBoxItem machin = new ListBoxItem();
+                    machin.Content = "Fire Alert issued";
+                    MessagesListBox.Items.Add(machin);  //jhvsdlhfs
+                }
+                else
+                {
+                    MessageBox.Show("Please select a screen to sent the message to", "Error, no screen selected", MessageBoxButton.OK);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An unexpected error occurred", "Error", MessageBoxButton.OK);
+            }
+        }
+
+        //publication préfaite d'un message d'urgence (alerte) sur le topic "emergency" du screen selectionné
+        private void GeneralEmergency_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (ScreenComboBox.SelectedItem != null)
+                {
+                    string cheminTopic = ScreenComboBox.SelectedItem.ToString() + "/emergency";
+                    string message = "GeneralEmergency";
+                    prout.PublishTopicMessage(cheminTopic, message);
+                    ListBoxItem machin = new ListBoxItem();
+                    machin.Content = "General Emergency issued";
+                    MessagesListBox.Items.Add(machin);
+                }
+                else
+                {
+                    MessageBox.Show("Please select a screen to sent the message to", "Error, no screen selected", MessageBoxButton.OK);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An unexpected error occurred", "Error", MessageBoxButton.OK);
+            }
+        }
+
+        //publication préfaite d'un message d'urgence (intrus) sur le topic "emergency" du screen selectionné
+        private void IntruderAlert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (ScreenComboBox.SelectedItem != null)
+                {
+                    string cheminTopic = ScreenComboBox.SelectedItem.ToString() + "/emergency";
+                    string message = "IntruderAlert";
+                    prout.PublishTopicMessage(cheminTopic, message);
+                    ListBoxItem machin = new ListBoxItem();
+                    machin.Content = "Intruder Alert issued";
+                    MessagesListBox.Items.Add(machin);
+                }
+                else
+                {
+                    MessageBox.Show("Please select a screen", "Error, no screen selected", MessageBoxButton.OK);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An unexpected error occurred", "Error", MessageBoxButton.OK);
+            }
         }
     }
 }
